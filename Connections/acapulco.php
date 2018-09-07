bd31af206df29f318c9fad6fe1526fb2b963766d
android-r9nd5p34 <franklin.benitez@loyola.edu.mx>
2018-09-07 14:04:35 -0500

    Login funcionando, crear, modificar y eliminar usuarios funcionando, diseño responsivo, menus animados, publicidad, admin index, admin, usuarios y otros detalles

--- Connections/acapulco.php
+++ Connections/acapulco.php
@@ -2,10 +2,11 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_acapulco = "127.0.0.1:80";
+$hostname_acapulco = "127.0.0.1";
 $database_acapulco = "acapulco";
 $username_acapulco = "developer";
 $password_acapulco = "testing";
-$acapulco = mysql_connect($hostname_acapulco, $username_acapulco, $password_acapulco) or trigger_error(mysql_error(),E_USER_ERROR); 
+/* Aqui pongo un @ antes de mysql para no mostrar la adevertencia */
+$acapulco = @mysql_pconnect($hostname_acapulco, $username_acapulco, $password_acapulco) or trigger_error(mysql_error(),E_USER_ERROR); 
 
 ?>
